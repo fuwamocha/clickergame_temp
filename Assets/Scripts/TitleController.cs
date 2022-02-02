e7d402dbfc8897cd4fc417074fd0cbb78f9616d8
fuwamocha <74049905+fuwamocha@users.noreply.github.com>
2022-02-02 23:32:01 +0900

    TitleManager.csの名称を変更し、AddListener()でクリック検知を行うように変更

---
+++ Assets/Scripts/TitleController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace ClickerGame {
+    public class TitleController : MonoBehaviour {
+
+        private void Start() {
+            var startButton = this.GetComponent<Button>();
+
+            startButton.onClick.AddListener(OnClickStartButton);
+        }
+
+        private void OnClickStartButton() {
+            SceneManager.LoadScene("Main");
+        }
+    }
+}
+
